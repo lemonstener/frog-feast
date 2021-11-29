1b262cb5eda430adcc16ad7a2355a634a45b9941
lemonstener <deyanvasilev7@gmail.com>
2021-11-29 11:40:20 -0600

    minor changes to the landing page

--- static/app.js
+++ static/app.js
@@ -1,159 +1,163 @@
 const lBox = {
-    caught: ''
-}
+  caught: "",
+};
 
 const rBox = {
-    caught: ''
-}
+  caught: "",
+};
 
-let score = 0
-let timeLeft = 29
+let score = 0;
+let timeLeft = 29;
 
-
-scoreCount.innerHTML = `${score}`
+scoreCount.innerHTML = `${score}`;
 
 function updateScore(bug) {
-    score += parseInt(e.bugInfo[bug].string)
-    if (score < 0) {
-        score = 0
-    }
-    scoreCount.innerHTML = `${score}`
+  score += parseInt(e.bugInfo[bug].string);
+  if (score < 0) {
+    score = 0;
+  }
+  scoreCount.innerHTML = `${score}`;
 }
 
-speaker.addEventListener('click', music)
+speaker.addEventListener("click", music);
 
 function startTimer() {
-    const timerInterval = setInterval(function() {
-        if (timeLeft < 11) {
-            timer.classList.add('heartbeat')
-        } else {
-            timer.classList.remove('heartbeat')
-        }
-        if (p.gameOver) {
-            timer.classList.remove('pulsate')
-            clearInterval(timerInterval)
-        }
-        timer.innerText = `${timeLeft}`
-        timeLeft--
+  const timerInterval = setInterval(function () {
+    if (timeLeft < 11) {
+      timer.classList.add("heartbeat");
+    } else {
+      timer.classList.remove("heartbeat");
+    }
+    if (p.gameOver) {
+      timer.classList.remove("pulsate");
+      clearInterval(timerInterval);
+    }
+    timer.innerText = `${timeLeft}`;
+    timeLeft--;
 
-        if (timeLeft < -1) {
-            timer.classList.remove('pulsate')
-            timer.innerText = 60
-            p.gameOver = true
-            clearInterval(timerInterval)
-            endGame()
-        }
-    }, 1000)
+    if (timeLeft < -1) {
+      timer.classList.remove("pulsate");
+      timer.innerText = 60;
+      p.gameOver = true;
+      clearInterval(timerInterval);
+      endGame();
+    }
+  }, 1000);
 }
 
 function startGame() {
-    landingPage.innerHTML = ''
-    html.classList.remove('background')
-    frog.className = `${p.direction}frog1`
-    heartField.innerHTML = `
+  landingPage.innerHTML = "";
+  html.classList.remove("background");
+  frog.className = `${p.direction}frog1`;
+  heartField.innerHTML = `
         <div class="heart"></div>
             <div class="heart"></div>
             <div class="heart"></div>
-        `
-    scoreField.hidden = false
-    timerField.hidden = false
-    score = 0
-    scoreCount.innerHTML = `${score}`
-    timer.innerText = `30`
-    timeLeft = 29
-    frog.hidden = false
-    p.gameOver = false
-    p.health = 3
-    p.stickingOutLeft = false
-    p.stickingOutRight = false
-    p.canPlay = true
-    p.health = 3
-    gameOver = false
-    speaker.hidden = false
-    landingPage.hidden = true
-    landingPage.style.right = '-9999px'
-    container.hidden = false
-    document.addEventListener('keydown', control)
-    startTimer()
-    createBug()
-    leftHalf.addEventListener('click', turnLeft)
-    rightHalf.addEventListener('click', turnRight)
-    if (p.gameInProgress && song.paused || !song.paused) {
-        return
-    }
-    music()
+        `;
+  scoreField.hidden = false;
+  timerField.hidden = false;
+  score = 0;
+  scoreCount.innerHTML = `${score}`;
+  timer.innerText = `30`;
+  timeLeft = 29;
+  frog.hidden = false;
+  p.gameOver = false;
+  p.health = 3;
+  p.stickingOutLeft = false;
+  p.stickingOutRight = false;
+  p.canPlay = true;
+  p.health = 3;
+  gameOver = false;
+  speaker.hidden = false;
+  landingPage.hidden = true;
+  landingPage.style.right = "-9999px";
+  container.hidden = false;
+  document.addEventListener("keydown", control);
+  startTimer();
+  createBug();
+  leftHalf.addEventListener("click", turnLeft);
+  rightHalf.addEventListener("click", turnRight);
+  if ((p.gameInProgress && song.paused) || !song.paused) {
+    return;
+  }
+  music();
 }
 
 function endGame() {
-    p.gameInProgress = true
-    const highScore = localStorage.getItem('highScore') || 0
-    let newRecord = false
-    if (score > highScore) {
-        localStorage.setItem('highScore', score)
-        newRecord = true
+  p.gameInProgress = true;
+  const highScore = localStorage.getItem("highScore") || 0;
+  let newRecord = false;
+  if (score > highScore) {
+    localStorage.setItem("highScore", score);
+    newRecord = true;
+  }
+  p.gameOver = true;
+  heartField.innerHTML = "";
+  scoreField.hidden = true;
+  timerField.hidden = true;
+  frog.hidden = true;
+  speaker.hidden = true;
+
+  leftHalf.removeEventListener("click", turnLeft);
+  rightHalf.removeEventListener("click", turnRight);
+
+  const gameOverPanel = document.createElement("div");
+  gameOverPanel.className = "game-over";
+
+  const backBTN = document.createElement("div");
+  backBTN.className = "back-btn";
+
+  const text = document.createElement("div");
+  if (newRecord) {
+    text.innerHTML = `New high score!!! <br> ${score}`;
+  } else {
+    text.innerHTML = `Final score: ${score} <br> (you once did ${highScore} though)`;
+  }
+
+  const restartBTN = document.createElement("span");
+  restartBTN.innerHTML = '<i class="fa fa-refresh fa-spin"></i>';
+  restartBTN.className = "reset-btn";
+
+  gameOverPanel.append(backBTN, text, restartBTN);
+  container.append(gameOverPanel);
+
+  restartBTN.addEventListener("click", function () {
+    gameOverPanel.remove();
+    startGame();
+  });
+
+  backBTN.addEventListener("click", function () {
+    gameOverPanel.remove();
+    container.hidden = true;
+    if (!song.paused) {
+      music();
     }
-    p.gameOver = true
-    heartField.innerHTML = ''
-    scoreField.hidden = true
-    timerField.hidden = true
-    frog.hidden = true
-    speaker.hidden = true
+    p.gameInProgress = false;
+    showLandingPage();
+  });
 
-    leftHalf.removeEventListener('click', turnLeft)
-    rightHalf.removeEventListener('click', turnRight)
-
-    const gameOverPanel = document.createElement('div')
-    gameOverPanel.className = 'game-over'
-
-    const backBTN = document.createElement('div')
-    backBTN.className = 'back-btn'
-
-    const text = document.createElement('div')
-    if (newRecord) {
-        text.innerHTML = `New high score!!! <br> ${score}`
-    } else {
-        text.innerHTML = `Final score: ${score} <br> (you once did ${highScore} though)`
-    }
-
-
-    const restartBTN = document.createElement('span')
-    restartBTN.innerHTML = '<i class="fa fa-refresh fa-spin"></i>'
-    restartBTN.className = 'reset-btn'
-
-    gameOverPanel.append(backBTN, text, restartBTN)
-    container.append(gameOverPanel)
-
-    restartBTN.addEventListener('click', function() {
-        gameOverPanel.remove()
-        startGame()
-    })
-
-    backBTN.addEventListener('click', function() {
-        gameOverPanel.remove()
-        container.hidden = true
-        if (!song.paused) { music() }
-        p.gameInProgress = false
-        showLandingPage()
-    })
-
-    setTimeout(function() {
-        gameOverPanel.style.marginTop = '20vh'
-    }, 300)
-
+  setTimeout(function () {
+    gameOverPanel.style.marginTop = "20vh";
+  }, 300);
 }
 
 function showLandingPage() {
-    landingPage.innerHTML = `
+  landingPage.innerHTML = `
     <div>
             <h1>FROG FEAST</h1>
             <h3><i>A.K.A the "What if the ATARI 2600 had 16bit graphics" game</i></h2>
             <p>Welcome to Frog Feast ! Eat some bugs, get a high score and then do it all over again!<br> Watch out for bees though! Eat 3 bees and it's game over! <br> Oh yeah, you have a time limit too. <br> Press
-                <span style="background-color: black; padding: 1px; border-radius: 5px; color:white">left</span> or
-                <span style="background-color: black; padding: 1px; border-radius: 5px; color:white">right</span> on the keyboard to stick your tongue out in that direction (or tap the screen on mobile).<br> Press the <span style="background-color: black; padding: 1px; border-radius: 5px; color:white">spacebar</span> to toggle the music on / off (or click on the
+                <span style="background-color: black; padding: 1.5px; border-radius: 5px; color:white">left</span> or
+                <span style="background-color: black; padding: 1.5px; border-radius: 5px; color:white">right</span> on the 
+                keyboard to stick your tongue out in that direction (or tap the screen on mobile).<br> 
+                Press the <span style="background-color: black; padding: 1.5px; border-radius: 5px; color:white">spacebar</span> 
+                to toggle the music on / off (or click on the
                 <span
-                    style="background-color: black; padding: 1px; border-radius: 5px; color:white">speaker</span>).
+                    style="background-color: black; padding: 1.5px; border-radius: 5px; color:white">speaker</span>).
                     <br>
-                    <span style="background-color: black; padding: 1px; border-radius: 5px; color:red">Flies</span> will get you 100 points, <span style="background-color: black; padding: 1px; border-radius: 5px; color:pink">Fluffly bugs</span> are worth 1000 and extend your timer but the <span style="background-color: black; padding: 1px; border-radius: 5px; color:gold">Bees</span> will hurt you.</p>
+                    <span style="background-color: black; padding: 1.5px; border-radius: 5px; color:red">Flies</span> will get you 100 points, 
+                    <span style="background-color: black; padding: 1.5px; border-radius: 5px; color:pink">Fluffly bugs</span> are worth 1000 and extend your 
+                    timer but the <span style="background-color: black; padding: 2px; border-radius: 5px; color:gold">Bees</span> will hurt you.</p>
                     <a href="https://www.bensound.com/royalty-free-music/2">Music by Bensound</a>
         </div>
 
@@ -163,15 +167,18 @@ function showLandingPage() {
             <div class="rdemo-frog"></div>
         </div>
         <div class="enemies">
-            <div style="margin-top:20px"><span style="background-color: black; padding: 1px; border-radius: 5px; color:red;">Fly</span></div>
+            <div style="margin-top:20px"><span style="background-color: black; padding: 2px; border-radius: 5px; color:red;">Fly</span></div>
             <div class="demo-fly"></div>
-            <div style="margin-top:20px"><span style="background-color: black; padding: 1px; border-radius: 5px; color:gold">Bee</span></div>
+            <div style="margin-top:20px"><span style="background-color: black; padding: 2px; border-radius: 5px; color:gold">Bee</span></div>
             <div class="demo-bee"></div>
-            <div style="margin-top:20px"><span style="background-color: black; padding: 1px; border-radius: 5px; color:pink">Fluffy Bug</span></div>
+            <div style="margin-top:20px"><span style="background-color: black; padding: 2px; border-radius: 5px; color:pink">Fluffy Bug</span></div>
             <div class="demo-puffer"></div>
         </div>
 
         <div class="start"></div>
+        <div style="background-color: rgba(51,51,51,.7); color:white; margin-top:3px;">
+        <p>Winner of the Mintbean Star Award for "exemplary achievement in the field of web development". </p>
+        </div>
         <div class="mob-links links">
                         <a href="https://www.linkedin.com/in/deyan-vasilev/">
                             <div class="linkedin"></div>
@@ -200,11 +207,11 @@ function showLandingPage() {
                 </div>
             </div>
         </div>
-    `
-    html.className = 'background'
-    const start = document.querySelector('.start')
-    start.addEventListener('click', startGame)
-    landingPage.hidden = false
+    `;
+  html.className = "background";
+  const start = document.querySelector(".start");
+  start.addEventListener("click", startGame);
+  landingPage.hidden = false;
 }
 
-showLandingPage()
+showLandingPage();
